037c5ed0545e63500cf342dbb686c6332b27ccfb
Anish Kumar <kumaranish200a@gmail.com>
2024-02-25 18:05:28 +0530

    btn functionality added

--- randomUser Api/script.js
+++ randomUser Api/script.js
@@ -1,59 +1,61 @@
-let url = "https://randomuser.me/api/";
-let data = fetch(url);
-// console.log(data);
-let card = document.querySelector(".card");
-data
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new error("Network not respomding");
-    }
-  })
-  .then((d) => {
-    console.log(d);
-    console.log(d.results[0]);
+const url = "https://randomuser.me/api/";
+const nextbtn = document.querySelector(".btnnext");
+function display() {
+  let data = fetch(url);
+  // console.log(data);
+  let card = document.querySelector(".card");
+  data
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new error("Network not respomding");
+      }
+    })
+    .then((d) => {
+      console.log(d);
+      // console.log(d.results[0]);
 
-    // gerder
-    let gender = d.results[0].gender;
-    console.log(gender);
+      // gerder
+      let gender = d.results[0].gender;
+      // console.log(gender);
 
-    // name
-    let tittle = d.results[0].name.title;
-    let first = d.results[0].name.first;
-    let last = d.results[0].name.last;
-    let FullName = `${tittle} ${first} ${last}`;
-    console.log(FullName);
+      // name
+      let tittle = d.results[0].name.title;
+      let first = d.results[0].name.first;
+      let last = d.results[0].name.last;
+      let FullName = `${tittle} ${first} ${last}`;
+      // console.log(FullName);
 
-    // email
-    const email = d.results[0].email;
-    console.log(email);
+      // email
+      const email = d.results[0].email;
+      // console.log(email);
 
-    // contact
-    const contact = d.results[0].phone;
-    console.log(contact);
+      // contact
+      const contact = d.results[0].phone;
+      // console.log(contact);
 
-    // date of birth
-    let age = d.results[0].dob.age;
-    console.log(age);
+      // date of birth
+      let age = d.results[0].dob.age;
+      // console.log(age);
 
-    // profile picture
-    let profilePic = d.results[0].picture.large;
-    console.log(profilePic);
+      // profile picture
+      let profilePic = d.results[0].picture.large;
+      // console.log(profilePic);
 
-    // address
+      // address
 
-    let city = d.results[0].location.city;
-    let country = d.results[0].location.country;
-    let state = d.results[0].location.state;
-    let street = d.results[0].location.street.number;
-    let streetName = d.results[0].location.street.name;
+      let city = d.results[0].location.city;
+      let country = d.results[0].location.country;
+      let state = d.results[0].location.state;
+      let street = d.results[0].location.street.number;
+      let streetName = d.results[0].location.street.name;
 
-    const address = `${street} ${streetName} ${city} ${state} ${country}`;
+      const address = `${street} ${streetName} ${city} ${state} ${country}`;
 
-    console.log(address);
+      // console.log(address);
 
-    const ihtml = `
+      const ihtml = `
     <div class="profilepic">
       <img src="${profilePic}" alt="profile">
     </div>
@@ -68,6 +70,10 @@ data
       </p>
     </div>
     `;
-    console.log(ihtml);
-    card.innerHTML = ihtml;
-  });
+      // console.log(ihtml);
+      card.innerHTML = ihtml;
+    });
+}
+
+nextbtn.addEventListener("click", display);
+display();
