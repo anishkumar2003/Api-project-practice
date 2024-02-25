b6cd322633afe1942278b898c21ca424c5f17a3e
Anish Kumar <kumaranish200a@gmail.com>
2024-02-25 16:40:30 +0530

    change btn added

--- jokes project api1/script.js
+++ jokes project api1/script.js
@@ -1,24 +1,29 @@
+const changeBtn = document.querySelector("#ChangeBtn");
 let url = "https://official-joke-api.appspot.com/random_joke";
-let data = fetch(url);
-data
-  .then((d) => {
-    if (d.ok) {
-      return d.json();
-    } else {
-      throw new Error("Network responding is working");
-    }
-  })
-  .then((d) => {
-    let card = document.querySelector(".card");
-    console.log(d);
-    let jokeid = d.id;
-    let joke = d.setup;
-    let punchline = d.punchline;
-    // console.log(joke, punchline);
+function jokechange() {
+  let data = fetch(url);
+  data
+    .then((d) => {
+      if (d.ok) {
+        return d.json();
+      } else {
+        throw new Error("Network responding is working");
+      }
+    })
+    .then((d) => {
+      let card = document.querySelector(".card");
+      console.log(d);
+      let jokeid = d.id;
+      let joke = d.setup;
+      let punchline = d.punchline;
+      // console.log(joke, punchline);
 
-    ihtml = `
+      ihtml = `
     <h2 class="MainJoke">${joke}</h2>
     <h3 class="JokePunchLine">${punchline}</h2>
     `;
-    card.innerHTML = ihtml;
-  });
+      card.innerHTML = ihtml;
+    });
+}
+changeBtn.addEventListener("click", jokechange);
+jokechange();
